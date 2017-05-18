a4f9d898801dcaac534b591b47d27a1bdf15d1c1
TaylorPzreal <taylorpzreal@gmail.com>
2017-05-18 08:17:27 +0800

    更新配置插件

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,23 @@
+import babel from 'rollup-plugin-babel';
+// import eslint from 'rollup-plugin-eslint';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
+
 export default {
   entry: 'src/js/embellish-image.js',
-  format: 'iife', // cjs , es iife, 
+  format: 'iife', // cjs , es , iife, 
   dest: 'dist/js/embellish-image.browser.js',
   moduleName: 'EmbellishImage',
+  plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
+    babel({exclude: 'node_modules/**'}),
+    // eslint({include: 'src/js/**'}),
+    uglify(),
+  ],
 };
--- src/example/test.js
+++ src/example/test.js
@@ -1,1 +1,15 @@
 console.warn('haha');
+
+function Person(name){
+  this.name = name;
+}
+
+Person.prototype.age = 24;
+
+var person  = new Person();
+
+person.age = 20;
+
+console.warn(person.constructor === Person);
+
+console.warn('uuu');
