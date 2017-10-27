7b74d40b7536402dac082a220aca30db38ee92d1
TaylorPzreal <taylorpzreal@gmail.com>
2017-10-28 05:55:37 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,51 @@
-import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript';
+import sass from 'rollup-plugin-sass';
+import autoprefixer from 'autoprefixer';
+import postcss from 'postcss';
 // import uglify from 'rollup-plugin-uglify';
 
 export default {
-  entry: 'src/js/embellish-image.js',
+  input: 'src/js/embellish-image.js',
 
-  // format: 'iife', // cjs , es , iife, 
-  // dest: 'dist/js/embellish-image.browser.js',
+  output: [
+    {
+      file: 'dist/embellish-image.js',
+      format: 'umd',
+      name: 'EmbellishImage',
+      sourcemap: true
+    },
+    {
+      file: 'dist/embellish-image.es.js',
+      format: 'es',
+      sourcemap: true
+    }
+  ],
 
-  // format: 'es',
-  // dest: 'dist/js/embellish-image.es.js',
-
-  format: 'cjs',
-  dest: 'dist/js/embellish-image.js',
-
-  moduleName: 'EmbellishImage',
   plugins: [
-    resolve({
-      jsnext: true,
-      main: true,
-      browser: true,
+    typescript({
+      typescript: require('typescript')
     }),
-    commonjs(),
-    babel({exclude: 'node_modules/**'}),
+    resolve({
+      module: true,
+      main: true
+      // browser: true,
+    }),
+    commonjs({
+      include: 'node_modules/**'
+    }),
+    sass({
+      processor: css =>
+        postcss([autoprefixer])
+          .process(css)
+          .then(result => result.css)
+    })
+    // babel({exclude: 'node_modules/**'}),
     // uglify(),
   ],
+  external: []
+
+  // onwarn: [],
+  // cache: []
 };
---
+++ tsconfig.ts
@@ -0,0 +1,19 @@
+{
+  "compileOnSave": false,
+  "compilerOptions": {
+    "target": "es5",
+    "module": "es2015",
+    "sourceMap": true,
+    "moduleResolution": "node",
+    "emitDecoratorMetadata": true,
+    "experimentalDecorators": true,
+    "declaration": true,
+    "outDir": "./dist",
+    "lib": ["es2017", "dom"],
+    "typeRoots": ["node_modules/@types"]
+  },
+  "files": ["./src"]
+  // "angularCompilerOptions": {
+  //   "skipTemplateCodegen": true
+  // }
+}
